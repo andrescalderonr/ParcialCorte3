9a5a20452c7a5f36a46473e3f1037ecced90080d
unknown <andres.calderon-r@labinfo.is.escuelaing.edu.co>
2025-05-23 15:15:42 -0500

    Primer commit

---
+++ src/main/java/edu/eci/cvds/parcial3c/Parcial3cApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.parcial3c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial3cApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parcial3cApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/cvds/parcial3c/Parcial3cApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.parcial3c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial3cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
